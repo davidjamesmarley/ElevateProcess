cbf020343c371197416a8208a3ac70f7ac93b463
David Marley <31593143+davidjamesmarley@users.noreply.github.com>
2022-01-31 23:34:27 +0100

    Created new solution from old utility code

---
+++ ElevateProcess/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ElevateProcess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: ep.exe <exe_path> [ <args> ] ");
+                Environment.Exit(1);
+            }
+
+            ProcessStartInfo startInfo = new ProcessStartInfo(args[0])
+            {
+                Verb = "runas"
+            };
+
+            if (args.Length > 1)
+            {
+                string[] psargs = new string[args.Length - 1];
+                Array.Copy(args, 1, psargs, 0, psargs.Length);
+                startInfo.Arguments = String.Join(" ", psargs);
+            }
+
+            Process.Start(startInfo);
+            Environment.Exit(0);
+        }
+    }
+}
